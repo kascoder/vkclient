1cd40d2db564588cc9c64a544a46fd8bc483be4f
kascoder <kascoder@gmail.com>
2020-05-02 11:15:25 +0300

    Audio object enhancements

--- src/main/java/io/kascoder/vkclient/domain/model/Audio.java
+++ src/main/java/io/kascoder/vkclient/domain/model/Audio.java
@@ -1,24 +1,28 @@
-// objects.json [audio_audio]
-
 package io.kascoder.vkclient.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.kascoder.vkclient.util.annotation.ApiObjectDefinition;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@JsonIgnoreProperties(ignoreUnknown = true) // duration, album_id, genre_id, performer
+@ApiObjectDefinition(name = "audio_audio")
+@JsonIgnoreProperties(ignoreUnknown = true) // album_id, genre_id, performer
 public class Audio {
     @JsonProperty("id")
     private Integer id;
+    @JsonProperty("owner_id")
+    private Integer owner_id;
     @JsonProperty("title")
     private String title;
     @JsonProperty("date")
-    private Integer dateUnixTime;
+    private Integer date; // UnixTime
     @JsonProperty("url")
-    private String url;
+    private String mp3Url;
     @JsonProperty("artist")
     private String artist;
+    @JsonProperty("duration")
+    private Integer duration; // seconds
 }
